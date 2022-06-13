d9ab45e1e0f005285102766d6160599e51cd80c8
Vijaykumar Maradana <email@vijaykumarmaradana2001@gmail.com>
2022-06-13 16:31:05 +0530

    class components

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Hey from './Component/Hello';
 // import { Hello } from './Component/Hello';
+import Welcome from './Component/Welcome';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
       <Hey/>
+      <Welcome/>
     </div>
   );
  }
---
+++ my-first-react-app/src/Component/Welcome.js
@@ -0,0 +1,11 @@
+// class components
+
+import React, {Component} from "react";
+
+class Welcome extends Component {
+    render() {
+        return <h3>Welcome, I am a class component</h3>
+    }
+}
+
+export default Welcome
