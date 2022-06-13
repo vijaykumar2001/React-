11d1cdd9c171fa24334e73f70914803e50adef79
Vijaykumar Maradana <email@vijaykumarmaradana2001@gmail.com>
2022-06-13 15:22:44 +0530

    functional components

---
+++ my-first-react-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Hey from './Component/Hello';
+// import { Hello } from './Component/Hello';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <Hey/>
+    </div>
+  );
+ }
+
+export default App;
---
+++ my-first-react-app/src/Component/Hello.js
@@ -0,0 +1,15 @@
+// functional component
+
+import React from "react";
+
+function Hello () {
+    return (
+        <div className = "App">
+            <h1>Hello, I am a functional Component</h1>
+        </div>
+    );
+}
+
+export default Hello;
+
+// export const Hello = () => <h2>Hello, I am a functional component</h2>
