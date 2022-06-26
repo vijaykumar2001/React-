f238926d115e203769d48b8f341b9dad8bcd1855
Vijaykumar Maradana <Vijaykumarmaradana2001@gmail.com>
2022-06-26 21:43:41 +0530

    Adding a new task

--- React Projects/todo-list/src/components/Todo.js
+++ React Projects/todo-list/src/components/Todo.js
@@ -8,13 +8,26 @@ class Todo extends Component{
         }
     }
 
+    handleChange = (e) => {
+        this.setState({
+            currTask : e.target.value
+        })
+    }
+
+    handleAddTask = () => {
+        this.setState({
+            tasks : [...this.state.tasks, this.state.currTask],
+            currTask : ""
+        })
+    }
+
     render(){
         return(
             <>
             {/* <div>This is a Todo component</div> */}
             <div>
-                <input type = "text"/>
-                <button>Add Task</button>
+                <input type = "text" value={this.state.currTask} onChange = {this.handleChange}/>
+                <button onClick={this.handleAddTask}>Add Task</button>
                 <ul>
                 {this.state.tasks.map((ele) => (
                     <li>
